79e6d20cc0259ff9aedf6e474d986e3737e5af9d
Krasimira Georgieva <krassi77bg@gmail.com>
2017-05-30 23:44:48 +0300

    Added more projects.

---
+++ 02. RectangleArea/02. RectangleArea.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class RectangleArea
+{
+    static void Main()
+    {
+        float width = float.Parse(Console.ReadLine());
+        float height = float.Parse(Console.ReadLine());
+        float result = width * height;
+
+        Console.WriteLine("{0:f2}", result);
+    }
+}
