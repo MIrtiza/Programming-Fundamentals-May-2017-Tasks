55f33c72df5d87862b2f2c550750c0c76be84388
Krasimira Georgieva <krassi77bg@gmail.com>
2017-06-17 08:50:17 +0300

    Add solutions from 01 to 05.

---
+++ Arrays Exercises/02. Rotate and Sum/02. Rotate and Sum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Rotate_and_Sum
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int rotations = int.Parse(Console.ReadLine());
+
+            int[] sumOfRotations = new int[numbers.Length];
+
+            for (int currentRotaions = 0; currentRotaions < rotations; currentRotaions++)
+            {
+                int lastElement = numbers[numbers.Length - 1];
+
+                for (int currentElement = numbers.
+                    Length-1; currentElement > 0; currentElement--)
+                {
+                    numbers[currentElement] = numbers[currentElement - 1];
+                }
+
+                numbers[0] = lastElement;
+
+                for (int currentElement = 0; currentElement < numbers.Length; currentElement++)
+                {
+                    sumOfRotations[currentElement] += numbers[currentElement];
+                }
+            }
+            Console.WriteLine(string.Join(" ", sumOfRotations));
+        }
+    }
+}
---
+++ Arrays Exercises/05. Compare Char Arrays/05. Compare Char Arrays.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.Compare_Char_Arrays
+{
+    class Program
+    {
+        static void Main()
+
+        {
+            char[] firstArray = Console.ReadLine().Split().Select(char.Parse).ToArray();
+            char[] secondArray = Console.ReadLine().Split().Select(char.Parse).ToArray();
+
+            int smallerArreyLenght = Math.Min(firstArray.Length, secondArray.Length);
+            bool areEqual = false;
+
+            for (int i = 0; i < smallerArreyLenght; i++)
+            {
+                if (firstArray[i] == secondArray[i])
+                {
+                    continue;
+                }
+                else
+                {
+                    if (firstArray[i] < secondArray[i])
+                    {
+                        Console.WriteLine(firstArray);
+                        Console.WriteLine(secondArray);
+                    }
+                    else
+                    {
+                        Console.WriteLine(secondArray);
+                        Console.WriteLine(firstArray);
+                    }
+                    areEqual = true;
+                    break;
+                }
+
+            }
+            if (!areEqual)
+            {
+                if (firstArray.Length <= secondArray.Length)
+                {
+                    Console.WriteLine(firstArray);
+                    Console.WriteLine(secondArray);
+                }
+                else
+                {
+                    Console.WriteLine(secondArray);
+                    Console.WriteLine(firstArray);
+                }
+            }
+        }
+
+    }
+}
