84333355a5f99221108eef258be125e52ea56a5a
Krasimira Georgieva <krassi77bg@gmail.com>
2017-06-28 23:47:09 +0300

    Add 05. Write to File solution.

---
+++ Objects, Classes, Files and Exceptios More Exercises/05. Write to File/05. Write to File.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace _05.Write_to_File
+{
+    class Program
+    {
+        static void Main()
+        {
+            var text = File.ReadAllText("sample_text.txt");
+            var chars = text.Where(c => ".,?!:".IndexOf(c) == -1).ToArray();
+
+            File.WriteAllText("output1.txt", string.Join("", chars));
+        }
+    }
+}
