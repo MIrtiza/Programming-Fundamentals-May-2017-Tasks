568bca8323e611514911e92344896ffd97ca8de4
Krasimira Georgieva <krassi77bg@gmail.com>
2017-07-03 20:37:52 +0300

    Add Strings and Text Processing solution.

---
+++ Strings and Text Processing Exercises/07. Multiply big number/07. Multiply big number.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+namespace _07.Multiply_big_number
+{
+    class Program
+    {
+        static void Main()
+        {
+            var firstNumber = Console.ReadLine().TrimStart('0');
+            var secondNumber = Console.ReadLine();
+
+            if (firstNumber == "0" || secondNumber == "0")
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            var inMind = 0;
+            var remainder = 0;
+
+            var result = new StringBuilder();
+
+            for (int i = firstNumber.Length - 1; i >= 0; i--)
+            {
+                for (int j = secondNumber.Length - 1; j >= 0; j--)
+                {
+                    var firstNumberParsed = int.Parse(firstNumber[i].ToString());
+                    var secondNumberParsed = int.Parse(secondNumber[j].ToString());
+
+                    var sum = firstNumberParsed * secondNumberParsed + inMind;
+
+                    inMind = sum / 10;
+                    remainder = sum % 10;
+
+                    result.Insert(0, remainder.ToString());
+
+                    if (i == 0 && inMind != 0)
+                    {
+                        result.Insert(0, inMind.ToString());
+                    }
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
