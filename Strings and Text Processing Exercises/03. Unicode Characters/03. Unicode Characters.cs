a569f5df29d03a970530a3d11ebcaf85c2f3bcb0
Krasimira Georgieva <krassi77bg@gmail.com>
2017-07-03 20:36:48 +0300

    Add Strings and Text Processing solution.

---
+++ Strings and Text Processing Exercises/03. Unicode Characters/03. Unicode Characters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _03.Unicode_Characters
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().ToCharArray();
+
+            foreach (var letter in input)
+            {
+                Console.Write($"\\u{(int)letter:x4}");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
