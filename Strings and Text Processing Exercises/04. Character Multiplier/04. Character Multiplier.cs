49d70c7789290f34a89c9787ce684684da70e8c2
Krasimira Georgieva <krassi77bg@gmail.com>
2017-07-03 20:37:05 +0300

    Add Strings and Text Processing solution.

---
+++ Strings and Text Processing Exercises/04. Character Multiplier/04. Character Multiplier.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _04.Character_Multiplier
+{
+    class Program
+    {
+        static void Main()
+        {
+            var strings = Console.ReadLine()
+            .Split();
+
+            var firstInput = strings[0];
+            var secondInput = strings[1];
+
+            var maxLength = Math.Max(firstInput.Length, secondInput.Length);
+
+            var totalCharactersSum = CalculateCharactersSum(firstInput, secondInput, maxLength);
+
+            Console.WriteLine(totalCharactersSum);
+        }
+
+        static long CalculateCharactersSum(string firstInput, string secondInput, int maxLength)
+        {
+            var totalSum = 0L;
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                if (firstInput.Length < i + 1)
+                {
+                    totalSum += secondInput[i];
+                }
+
+                else if (secondInput.Length < i + 1)
+                {
+                    totalSum += firstInput[i];
+                }
+
+                else
+                {
+                    totalSum += firstInput[i] * secondInput[i];
+                }
+            }
+
+            return totalSum;
+        }
+    }
+}
