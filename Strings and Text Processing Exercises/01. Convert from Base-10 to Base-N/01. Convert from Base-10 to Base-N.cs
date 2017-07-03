ec3961630cb59ac727cd45e1b857659654421685
Krasimira Georgieva <krassi77bg@gmail.com>
2017-07-03 20:35:59 +0300

    Add Strings and Text Processing solution.

---
+++ Strings and Text Processing Exercises/01. Convert from Base-10 to Base-N/01. Convert from Base-10 to Base-N.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace _01.Convert_from_Base_10_to_Base_N
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split().ToArray();
+            var number = BigInteger.Parse(input[1]);
+            var toBase = int.Parse(input[0]);
+            
+            var result = ConvertFromDecimalToBaseN(number,toBase);
+            Console.WriteLine(result);
+        }
+
+         static string ConvertFromDecimalToBaseN(BigInteger number, int toBase)
+        {
+            var result = string.Empty;
+            while(number > 0)
+            {
+                var remainder = number % toBase;
+
+                result = remainder + result;
+                number /= toBase;
+            }
+
+            return result;
+        }
+    }
+}
